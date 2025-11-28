9ca463603391b37ba95f7625740b6f9577b5f1d5
DJ <djsaez6@gmail.com>
2025-11-28 16:23:07 -0500

    Setting up repo environment with express (#5)
    * setting up environment
    
    * setting up server.js and src folder

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+// entry-point to app
+
+// load .env variables
+require('dotenv').config();
+
+// import packages
+const express = require('express');
+
+// create express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.static('public'));
+
+// TODO: will eventually import and use routes
+
+
+// testing
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+// start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
