c6c68f8dfc9d0fbf556420fedda8ad0cdcd18633
DJ <djsaez6@gmail.com>
2025-12-01 18:44:05 -0500

    adding clean URL routes
    - Added some logic so that when we start the app, it goes to index.html (originally John's api file) automatically
    - Instead of navigating to /index.html, /login.html, and '/signup.html', we now go to '/', '/login', and 'signup'
    - Start the application by running 'node server.js' in the terminal

--- server.js
+++ server.js
@@ -7,21 +7,22 @@ require('dotenv').config();
 // import packages
 const express = require('express');
 const db = require('./src/database/connection.js');
+const pagesRouter = require('./src/routes/pageRoutes.js')
 
 // create express app
 const app = express();
 
-// middleware
+// middleware to parse json and form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve static files from 'public' folder. Without this,
+// browser doesn't know where to find the linked CSS and JS 
+// files inside our html
 app.use(express.static('public'));
 
-// TODO: will eventually import and use routes
-
-
-// testing
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+// use pages router so we don't have .html in the URL
+app.use('/', pagesRouter);
 
 // start server
 const PORT = process.env.PORT || 3000;
