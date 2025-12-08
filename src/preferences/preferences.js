c90c479e7f61808ed991bbd1b3fcd3b1567f3b5a
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2025-12-08 15:07:08 -0500

    implemented getUserPreference

--- src/preferences/preferences.js
+++ src/preferences/preferences.js
@@ -5,12 +5,13 @@ const functions  = {
     // // add a preference to user
     // async addUserPreference(preference, value) {
         
-    // }
+    // }    
 
     async getUserPreference(user, preference) {
         const sql = "SELECT ?           \
                      FROM   preferences \
-                     WHERE  user_id = user, "
+                     WHERE  user_id = ? ";
+        return db.query(sql, [preference, user]);
     }
 };
 
