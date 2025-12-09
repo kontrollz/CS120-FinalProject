2835f79de75b846b7791354154168e19fa112b20
Helena Simson <68454497+ArkfallBravo@users.noreply.github.com>
2025-12-09 10:18:53 -0500

    cleaned up code

--- src/preferences/preferences.js
+++ src/preferences/preferences.js
@@ -2,11 +2,6 @@
 const db = require('../database/connection.js');
 
 const functions  = {
-    // // add a preference to user
-    // async addUserPreference(preference, value) {
-        
-    // }    
-
     async getUserPreference(user, preference) {
         const sql = "SELECT ?           \
                      FROM   preferences \
